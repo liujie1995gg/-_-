741ec9976d3f40b66459dc9ae08322ccb05b0b2d
liujie1995gg <38413517+liujie1995gg@users.noreply.github.com>
2018-09-18 09:29:51 +0800

    时间转换简单模块
    第一次上传，之后再改

---
+++ time_str.py
@@ -0,0 +1,19 @@
+import time,datetime
+
+def show_time(times=None):
+    '''#返回当前格式化时间，文本型
+    如果time = None,则取当前时间'''
+    if times == None:
+        times = time.strftime( '%Y-%m-%d %H:%M:%S',time.localtime())
+        return times#返回时间文本型
+    else:
+        times = datetime.datetime.strptime(times,'%Y-%m-%d %H:%M:%S')
+        return times
+
+a = show_time('2018-09-17 09:38:58')
+print(a)
+b = show_time()
+b = show_time(b)
+print(b)
+print(b -a)
+
